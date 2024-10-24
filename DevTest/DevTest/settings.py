9617a904bf15a9db4735665ebc49d3ec8196e564
Hari-om07 <154863625+Hari-om07@users.noreply.github.com>
2024-10-24 11:15:30 +0530

    Update settings.py

--- DevTest/DevTest/settings.py
+++ DevTest/DevTest/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ['medius-technologies-0am6.onrender.com', 'localhost', '127.0.0.
 # Application definition
 
 INSTALLED_APPS = [
-    'DevTest.uploadfile',
+    'DevTest.DevTest.uploadfile',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
